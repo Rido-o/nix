548e5b509792020151d076a18015f6f38ed3291a
Rido <rido@airmail.cc>
2024-02-10 11:16:35 +1100

    consolidate suckless overlays into one flake

--- overlays/flake.nix
+++ overlays/flake.nix
@@ -2,19 +2,14 @@
   description = "Flake containing all of my overlays";
 
   inputs = {
-    dwm.url = "path:./dwm";
-    xmenu.url = "path:./xmenu";
-    dmenu.url = "path:./dmenu";
+    suckless.url = "path:./suckless";
     nsxiv-rifle.url = "path:./nsxiv-rifle";
   };
 
   outputs = inputs@{ self, ... }: {
     overlays = with inputs; [
-      dwm.overlay
-      xmenu.overlay
-      # dmenu.overlay
       nsxiv-rifle.overlays.default
-      (import ./st)
+      suckless.overlay
     ];
   };
 }
